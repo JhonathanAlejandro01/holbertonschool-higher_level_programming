7d2992558ceaef5fd5692675144778485a8f45eb
JhonathanAlejandro01 <joaleanco@hotmail.com>
2020-04-29 22:21:44 -0500

    3 tired 3

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(8):
-    for x in range (10):
+    for x in range(10):
         if i < x:
             print("{:d}{:d}".format(i, x), end=', ')
 print("{}".format(89))
