a7f9df36cea4c456d9102e89f4a1349744f5717b
JhonathanAlejandro01 <joaleanco@hotmail.com>
2020-05-07 22:08:01 -0500

    1 & .gitignore

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    list2 = []
+    for i in my_list:
+        if i == search:
+           list2.append(replace)
+        else:
+            list2.append(i)
+    return (list2)
