02e09de3a92330d201fa3fe1f330ef2f399aa93a
JhonathanAlejandro01 <joaleanco@hotmail.com>
2020-05-07 22:10:40 -0500

    fix pep8

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,7 +3,7 @@ def search_replace(my_list, search, replace):
     list2 = []
     for i in my_list:
         if i == search:
-           list2.append(replace)
+            list2.append(replace)
         else:
             list2.append(i)
     return (list2)
