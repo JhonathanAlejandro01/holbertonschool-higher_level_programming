23265f98c6eef6da1a429e3964ee3e0c837e2d8e
JhonathanAlejandro01 <joaleanco@hotmail.com>
2020-08-24 22:01:20 -0500

    fix task 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
 """
-Python script that takes in a URL, sends a request to the URL
-and displays the body of the response.
+script that takes in a URL and an email address, sends a POST request
+to the passed URL with the email as a parameter, and finally displays
+the body of the response.
 """
-
 import requests
 import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    error = requests.get(url)
-    if error.status_code == 200:
-        print(error.text)
-    else:
-        print("Error code: {}".format(error.status_code))
+    mail = requests.post(url, data={'email': sys.argv[2]})
+    print(mail.text)
